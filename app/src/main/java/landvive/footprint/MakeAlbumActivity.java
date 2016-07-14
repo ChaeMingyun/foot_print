e2959db2773b3ad7ed1d1d975bce6dc83317de60
SongWonGeun <rmr123@naver.com>
2016-07-14 19:14:05 +0900

    base form

---
+++ app/src/main/java/landvive/footprint/MakeAlbumActivity.java
@@ -0,0 +1,16 @@
+package landvive.footprint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by 송원근 on 2016-07-12.
+ */
+public class MakeAlbumActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makealbum);
+    }
+}
